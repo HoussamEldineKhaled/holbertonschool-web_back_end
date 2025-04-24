dfac4621f2f5bdca2d73ec44b474cbbd630427bf
root <houssam.khaled@lau>
2025-04-24 19:27:38 +0000

    fixed lol

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
+"""adding with annotation"""
+
+
 def add(a: float, b: float) -> float:
+    """add function
+
+    Args:
+       a: float
+       b: float
+
+    Returns: sum
+    """
     return a + b
