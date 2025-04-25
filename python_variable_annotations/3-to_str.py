8ca2ba0e1a412f60ebce586cdf67d4bb0d7d15bb
root <houssam.khaled@lau.edu>
2025-04-25 15:56:54 +0000

    turn to str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""to string"""
+
+
+def to_str(n: float) -> str:
+    """turn variable to string
+
+    Args:
+       n: number input
+
+    Returns: a string
+    """
+    return str(n)
