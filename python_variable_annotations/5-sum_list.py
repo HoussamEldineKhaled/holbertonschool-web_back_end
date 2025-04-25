f2059acb7d629b99fd2d117ba4c177678fb00fbc
root <houssam.khaled@lau.edu>
2025-04-25 17:06:20 +0000

    float sums

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""list of floats"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+
+    Args:
+       input_list: list of floats
+
+    Returns: sum of floats
+    """
+    sum = 0
+    for i in range(0, len(input_list)):
+        sum += input_list[i]
+    return sum
