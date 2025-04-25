571576c0a410920f73958f3d1b3dd2413917db2e
root <houssam.khaled@lau.edu>
2025-04-25 12:50:02 +0000

    concatination

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -3,4 +3,12 @@
 
 
 def concat(str1: str, str2: str) -> str:
+    """concatination 
+
+    Args:
+       str1: first string
+       str2: second string
+
+    Returns: concatenated string
+"""
     return str1 + str2
