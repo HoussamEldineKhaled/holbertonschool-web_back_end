d7783f913b381942347cda0d4a4331886698e22d
root <houssam.khaled@lau.edu>
2025-04-27 16:30:58 +0000

    tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 """complex string and int float to tuple"""
 
-from typing import Union, List
-def to_kv(k: str, v: Union[float, int]) -> List[]:
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+    """
+
+    Args:
+       k: string
+       v float or integer
+
+    Returns: a tuple
+    """
+    return (str, float(v ** 2))
