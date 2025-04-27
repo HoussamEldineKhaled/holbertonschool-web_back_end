b6b50ff867ecdde4ba9c0a81e2e0e7386eb41127
root <houssam.khaled@lau.edu>
2025-04-27 07:54:20 +0000

    mixed sum

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,8 +2,15 @@
 """summation of a mixed list"""
 
 
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: ):
-    
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """sum a mixed list
+
+    Args:
+       mxd_lst: mixed list of floats and ints
+
+    Returns: summations
+    """
+    return sum(mxd_lst)
