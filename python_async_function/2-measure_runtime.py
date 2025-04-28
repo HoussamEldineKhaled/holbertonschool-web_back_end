2b7ea09635bf3b5d481c434f0bc1caa6265961eb
root <houssam.khaled@lau.edu>
2025-04-28 17:11:37 +0000

    badde nem

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -4,3 +4,22 @@
 
 import asyncio
 import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """measure the time
+
+    Args:
+       n: number of coroutines to execute
+       max_delay: maximum delay for each coroutine
+
+    Returns: Average execution time per coroutine
+    """
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+
+    total_time = end - start
+    return total_time / n
