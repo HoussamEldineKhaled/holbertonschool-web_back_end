1e38cfe4f336186f869137e47fca2d7e5d58c459
root <houssam.khaled@lau.edu>
2025-04-28 14:18:20 +0000

    first async

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,13 +6,7 @@ import asyncio, random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """
-
-    Args:
-       max_delay: maximum delayy
-
-    Returns: the delay
-    """
+    """wait for delay"""
     delay = random.uniform(0, max_delay + 1)
     await asyncio.sleep(delay)
     return delay
