c7e4c656413198cdda449c6770f641fbd15edc84
root <houssam.khaled@lau.edu>
2025-05-01 12:43:47 +0000

    comprehension

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""measure the run time"""
+
+
+import asyncio
+import time
+from typing import List
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure the runtime
+
+    Returns: total runtime
+    """
+
+    start = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end = time.time()
+    return end - start
