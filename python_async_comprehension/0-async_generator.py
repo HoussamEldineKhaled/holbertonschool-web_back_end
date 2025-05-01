738692c5382451bfb8360eb2ab6007d205c63695
root <houssam.khaled@lau.edu>
2025-05-01 09:07:30 +0000

    please work

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,4 +7,5 @@ from typing import Generator
 async def async_generator() -> Generator[float, None, None]:
     """asynchronous generator"""
     for _ in range(10):
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
