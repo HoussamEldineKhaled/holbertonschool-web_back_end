24c312a4de952f4c1cdd9d3e1ed733af4e0831b9
root <houssam.khaled@lau.edu>
2025-05-01 10:49:27 +0000

    comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,8 +2,10 @@
 """asynchronous comprehension"""
 
 
-async_generator = __import__('0-async_generator').async_generator
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
 
 async def async_comprehension() -> List[float]:
     """async comprehensions"""
