3289a1be22141666ca7b6d765ec488c3a10b447b
root <houssam.khaled@lau>
2025-05-03 17:14:16 +0000

    list topics

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""return list of school with topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+
+    Args:
+       mongo_collection: collection
+       topic: topic in question
+
+    Returns: list of topics
+    """
+    return list(mongo_collection.find("topics" : topic))
