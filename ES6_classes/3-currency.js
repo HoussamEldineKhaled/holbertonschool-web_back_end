f9af8a8987fe3846756bc7e65f727a804dd70b39
root <houssam.khaled@lau.edu>
2025-06-01 16:05:41 +0000

    lol

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -4,22 +4,22 @@ class Currency {
     this._name = name;
   }
 
-  get code(){
+  get code() {
     return this._code;
   }
 
-  set code(value){
+  set code(value) {
     this._code = value;
   }
 
-  get name(){
+  get name() {
     return this._name;
   }
 
-  set name(value){
+  set name(value) {
     this._name = value
   }
-  displayFullCurrency(){
+  displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
 }
