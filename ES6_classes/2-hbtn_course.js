ec1ff602b45c14ff819a8cd2ee439aa313aa0c44
root <houssam.khaled@lau.edu>
2025-06-01 14:56:00 +0000

    lol

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -21,7 +21,7 @@ class HolbertonCourse {
     }
 
     set name(value){
-        if (typeof name !== 'string') {
+        if (typeof value !== 'string') {
            throw new TypeError('Name must be a string');
         }
 	this._name = value;
@@ -32,7 +32,7 @@ class HolbertonCourse {
     }
 
     set length(value){
-        if (typeof length !== 'number') {
+        if (typeof value !== 'number') {
             throw new TypeError('Length must be a number');
         }
 	this._length = value;
@@ -42,7 +42,7 @@ class HolbertonCourse {
 	return this._students
     }
     set students(value){
-        if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+        if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
             throw new TypeError('Students must be an array of strings');
         }
 	this._students = value;
