bcd57497077fbb7821df423f42dda13e07aff6c4
root <houssam.khaled@lau.edu>
2025-06-01 13:24:46 +0000

    lol

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,8 +1,5 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-
-const initializeRooms = () => {
-    return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-};
+const initializeRooms = () => [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 
 export default initializeRooms;
