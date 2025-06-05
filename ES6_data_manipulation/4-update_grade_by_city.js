5c0af6a032aed5f78ba431e26c56dea968e86dce
root <houssam.khaled@lau.edu>
2025-06-05 17:43:21 +0000

    lol

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,11 @@
 function updateStudentGradeByCity(studentList, city, newGrades) {
-    return studentList.filter(student => student.location === city).map(
-	student => {
-	    const grading = newGrades.find(grade => grade.studentId === student.id);
+  return studentList.filter((student) => student.location === city).map(
+    (student) => {
+      const grading = newGrades.find((grade) => grade.studentId === student.id);
 
-	    return {...student, grade: grading ? grading.grade : 'N/A'};
-	}
-    );
-};
+      return { ...student, grade: grading ? grading.grade : 'N/A' };
+    },
+  );
+}
 
 export default updateStudentGradeByCity;
