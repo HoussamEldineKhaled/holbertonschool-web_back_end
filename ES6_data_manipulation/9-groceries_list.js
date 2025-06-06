af80d18172531fb17dec044de1db01b5b6e7c644
root <houssam.khaled@lau.edu>
2025-06-06 11:46:56 +0000

    lol

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,5 +1,5 @@
 function groceriesList() {
-    return new Map([['Apple', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]]);
+    return new Map([['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]]);
 }
 
 export default groceriesList;
