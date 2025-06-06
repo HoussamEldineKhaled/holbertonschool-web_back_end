8fd73bac125bac19cfc6a413b115553c80e6a896
root <houssam.khaled@lau.edu>
2025-06-06 11:10:55 +0000

    lol

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,20 @@
+function cleanSet(set, startString) {
+    if (!startString || startString === '') {
+	return '';
+    }
+
+    const filteredValue = [];
+
+    for (const value of set) {
+	if (typeof value === 'string' && value.startsWith(startString)) {
+	    const remainingPart = value.slice(startString.length);
+	    if (remainingPart !== '') {
+		filteredValue.push(remainingPart);
+	    }
+	}
+    }
+
+    return filteredValue.join('-');
+}
+
+export default cleanSet;
