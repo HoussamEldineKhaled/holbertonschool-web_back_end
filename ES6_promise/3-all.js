1d255c8773e1cf2ef1935b995d25cd99226c692e
root <houssam.khaled@lau.edu>
2025-06-08 09:05:32 +0000

    lol

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,11 +2,10 @@ import {uploadPhoto, createUser} from "./utils"
 
 export default function handleProfileSignup() {
     Promise.all([uploadPhoto(), createUser()])
-	.then((values) => {
-	    const [photo, user] = values;
+	.then(([photo, user]) => {
 	    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
 	})
 	.catch(() => {
-	    console.error("Signup system offline");
+	    console.log("Signup system offline");
 	});
 }
