396d7aca514be36b4dbda67309019100b28cf76e
root <houssam.khaled@lau.edu>
2025-06-16 19:40:39 +0000

    lol

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 """many coroutines"""
 
-
 import asyncio
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
-
-
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """wait n times and record delay times"""
