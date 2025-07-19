a046281f3f0a14ac3396b442cf927bcb3173802b
root <you@example.com>
2025-07-19 16:43:28 +0000

    lol

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,9 +6,6 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  process.stdout.write('This important software is now closing\n');
-});
-
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
 });
