cfb5a7a20d63fb5d5fc5124d18c2609f71d5b3a4
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2025-07-20 18:08:24 +0300

    lol

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,13 +1,35 @@
-
 const fs = require('fs');
 
-function countStudents(path){
-    try{
+function countStudents(path) {
+  try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.trim.split('\n');
-    const nb = lines.length - 1;
-    console.log(`Number of students: ${nb}`);
-    } catch (e) {
-    throw new Error('Cannot load the database');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const studentLines = lines.slice(1);
+
+    console.log(`Number of students: ${studentLines.length}`);
+
+    const fieldGroups = {};
+
+    studentLines.forEach((line) => {
+      const column = line.split(',');
+
+      if (column.length === 4) {
+        const firstName = column[0].trim();
+        const field = column[3].trim();
+
+        if (!fieldGroups[field]) {
+          fieldGroups[field] = [];
+        }
+        fieldGroups[field].push(firstName);
+      }
+    });
+
+    for (const [field, students] of Object.entries(fieldGroups)) {
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
     }
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
 }
+
+module.exports = countStudents;
