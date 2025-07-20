c504b5b6f08c466579328731f086c512e5b152a6
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2025-07-20 20:24:00 +0300

    lol

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf-8');
+    const data = fs.readFile(path, 'utf-8');
     const lines = data.split('\n').filter((line) => line.trim() !== '');
     const studentLines = lines.slice(1);
 
