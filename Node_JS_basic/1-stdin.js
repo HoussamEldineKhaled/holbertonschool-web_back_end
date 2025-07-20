96332163f5c2a6c1543f3bdb43b949dcd4918b3b
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2025-07-20 14:16:48 +0300

    lol

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,14 @@
-/**
-   * user's name asking
- */
 process.stdin.setEncoding('utf8');
-
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
+
